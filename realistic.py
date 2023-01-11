1fb9c6da345f4a655e5fefea40bc2c667af02db9
Juan Carlos Martinez <jm2638@cornell.edu>
2023-01-11 14:20:20 -0500

    .

--- realistic.py
+++ realistic.py
@@ -4,9 +4,10 @@ import numpy as np
 import osmnx as ox
 import pickle
 import pandas as pd
-import plotly.express as px
+import plotly.graph_objects as go
 import swifter  # parallelize pandas apply() function
 from datetime import datetime
+from plotly.subplots import make_subplots
 from solver import steiner_forest
 
 
@@ -138,10 +139,10 @@ def main():
         pre_process()
 
     print('     loading data')
-    with open('./g.pkl', 'rb') as file:
+    with open('./realistic/g.pkl', 'rb') as file:
         g = pickle.load(file)
-    g_dists = pd.read_csv('./g_dists.csv', index_col=0).rename(columns=lambda x: int(x))
-    df = pd.read_csv('./df.csv', parse_dates=['s_dt', 't_dt'])
+    g_dists = pd.read_csv('./realistic/g_dists.csv', index_col=0).rename(columns=lambda x: int(x))
+    df = pd.read_csv('./realistic/df.csv', parse_dates=['s_dt', 't_dt'])
 
     mst_cost = sum(data['length'] for _, _, data in nx.minimum_spanning_tree(g, weight='length').edges(data=True))
 
@@ -155,9 +156,9 @@ def main():
         job_ct = 0
         job_mst_cost = mst_cost
         job_opt = None
-        job_a = None
+        job_theta = None
 
-        results.append([job_delta_t, job_rho, job_g, job_ct, job_mst_cost, job_opt, job_a])
+        results.append([job_delta_t, job_rho, job_g, job_ct, job_mst_cost, job_opt, job_theta])
 
         for _, _, data in job_g.edges(data=True):
             data['edge_ct'] = 0
@@ -185,202 +186,134 @@ def main():
 
                 results[-1] = [job_delta_t, job_rho, job_g, job_ct, job_mst_cost, job_opt, job_a]
 
-                results_df = pd.DataFrame(results, columns=['delta_t', 'rho', 'g', 'ct', 'mst_cost', 'opt', 'a'])
+                results_df = pd.DataFrame(results, columns=['delta_t', 'rho', 'g', 'ct', 'mst_cost', 'opt', 'theta'])
 
                 pd.to_pickle(results_df, './realistic/results_df.pkl')
 
 
-if __name__ == '__main__':
-    # main()
-    pass
+def curve_plotter():
 
-#
-#
-results_df_new = pd.read_pickle('./results_nyc/results_df_new_new.pkl')
-results_df = pd.read_pickle('./results_nyc/results_df_new.pkl')
-results_df = results_df[results_df['delta_t'] < 600]
-results_df = pd.concat([results_df, results_df_new])
-results_df.sort_values('delta_t', inplace=True)
-results_df['delta_t'] /= 60
-results_df['numerator'] = results_df['a'] * results_df['delta_t']
+    results_df = pd.read_pickle('./realistic/results_df.pkl')
+    results_df.sort_values('delta_t', inplace=True)
+    results_df['delta_t'] /= 60
+    results_df['numerator'] = results_df['theta'] * results_df['delta_t']
 
-w = 1800 * 0.8
-h = 600
+    w = 1800 * 0.8
+    h = 600
 
-from plotly.subplots import make_subplots
-import plotly.graph_objects as go
+    fig = make_subplots(
+        rows=1, cols=3, shared_xaxes=True, horizontal_spacing=w/40000, vertical_spacing=h/10000,
+        column_titles=[r'$\huge{\rho} = 5/4$', r'$\huge{\rho} = 4/3$', r'$\huge{\rho} = 3/2$']
+    )
 
-delta_list = np.linspace(0.15, 20, 40)
+    results_df['data'] = results_df.apply(
+        lambda x: [(delta_s, x.numerator / delta_s) for delta_s in np.arange(int(x.delta_t), 30, 0.1) if x.numerator / delta_s >= 1],
+        axis=1
+    )
+    #
+    results_df_125 = results_df[results_df['rho'] == 1.25]
+    results_df_133 = results_df[results_df['rho'] == 4/3]
+    results_df_150 = results_df[results_df['rho'] == 1.5]
 
-fig = make_subplots(
-    rows=1, cols=3, shared_xaxes=True, horizontal_spacing=w/40000, vertical_spacing=h/10000,
-    column_titles=[r'$\huge{\rho} = 5/4$', r'$\huge{\rho} = 4/3$', r'$\huge{\rho} = 3/2$']
-)
+    deltas = {1, 3, 6, 10, 12, 15}
+    colors = ['#000000', '#a9a9a9', '#4363d8', '#000000', '#a9a9a9', '#4363d8']
+    dashes = ['solid', 'solid', 'solid', 'dot', 'dot', 'dot']
 
-results_df['data'] = results_df.apply(
-    lambda x: [(delta_s, x.numerator / delta_s) for delta_s in np.arange(int(x.delta_t), 30, 0.1) if x.numerator / delta_s >= 1],
-    axis=1
-)
-#
-results_df_125 = results_df[results_df['rho'] == 1.25]
-results_df_133 = results_df[results_df['rho'] == 4/3]
-results_df_150 = results_df[results_df['rho'] == 1.5]
+    idx = 0
+    for i, row in results_df_125.iterrows():
+        if row.delta_t not in deltas:
+            continue
+        delta_s, estimate = list((zip(*row.data)))
+        fig.add_trace(go.Scatter(
+            x=delta_s,
+            y=estimate,
+            name=r'$\delta_d = {0}$'.format(row.delta_t),
+            marker={'color': colors[idx]},
+            mode='lines',
+            line=dict(dash=dashes[idx]),
+            showlegend=True
+        ), row=1, col=1)
+        idx += 1
 
-deltas = {1, 3, 6, 10, 12, 15}
-colors = ['#000000', '#a9a9a9', '#4363d8', '#000000', '#a9a9a9', '#4363d8']
-dashes = ['solid', 'solid', 'solid', 'dot', 'dot', 'dot']
+    idx = 0
+    for i, row in results_df_133.iterrows():
+        if row.delta_t not in deltas:
+            continue
+        delta_s, estimate = list((zip(*row.data)))
+        fig.add_trace(go.Scatter(
+            x=delta_s,
+            y=estimate,
+            name=r'$\delta_d = {0}$'.format(row.delta_t),
+            marker={'color': colors[idx]},
+            mode='lines',
+            line=dict(dash=dashes[idx]),
+            showlegend=False
+        ), row=1, col=2)
+        idx += 1
 
-idx = 0
-for i, row in results_df_125.iterrows():
-    if row.delta_t not in deltas:
-        continue
-    delta_s, estimate = list((zip(*row.data)))
-    fig.add_trace(go.Scatter(
-        x=delta_s,
-        y=estimate,
-        name=r'$\delta_d = {0}$'.format(row.delta_t),
-        marker={'color': colors[idx]},
-        mode='lines',
-        line=dict(dash=dashes[idx]),
-        showlegend=True
-    ), row=1, col=1)
-    idx += 1
+    idx = 0
+    for i, row in results_df_150.iterrows():
+        if row.delta_t not in deltas:
+            continue
+        delta_s, estimate = list((zip(*row.data)))
+        fig.add_trace(go.Scatter(
+            x=delta_s,
+            y=estimate,
+            name=r'$\delta_d = {0}$'.format(row.delta_t),
+            marker={'color': colors[idx]},
+            mode='lines',
+            line=dict(dash=dashes[idx]),
+            showlegend=False
+        ), row=1, col=3)
+        idx += 1
 
-idx = 0
-for i, row in results_df_133.iterrows():
-    if row.delta_t not in deltas:
-        continue
-    delta_s, estimate = list((zip(*row.data)))
-    fig.add_trace(go.Scatter(
-        x=delta_s,
-        y=estimate,
-        name=r'$\delta_d = {0}$'.format(row.delta_t),
-        marker={'color': colors[idx]},
-        mode='lines',
-        line=dict(dash=dashes[idx]),
-        showlegend=False
-    ), row=1, col=2)
-    idx += 1
+    fig.update_yaxes(range=[-0.125, 5.625], row=1, col=1)
+    fig.update_yaxes(range=[-0.125, 5.625], row=1, col=2)
+    fig.update_yaxes(range=[-0.125, 5.625], row=1, col=3)
+    fig.add_hline(y=1, line_width=1, line_dash="dash", line_color="green", row=1, col=1)
+    fig.add_hline(y=1, line_width=1, line_dash="dash", line_color="green", row=1, col=2)
+    fig.add_hline(y=1, line_width=1, line_dash="dash", line_color="green", row=1, col=3)
 
-idx = 0
-for i, row in results_df_150.iterrows():
-    if row.delta_t not in deltas:
-        continue
-    delta_s, estimate = list((zip(*row.data)))
-    fig.add_trace(go.Scatter(
-        x=delta_s,
-        y=estimate,
-        name=r'$\delta_d = {0}$'.format(row.delta_t),
-        marker={'color': colors[idx]},
-        mode='lines',
-        line=dict(dash=dashes[idx]),
-        showlegend=False
-    ), row=1, col=3)
-    idx += 1
+    fig.update_xaxes(range=[0, 21], row=1, col=1)
+    fig.update_xaxes(range=[0, 21], row=1, col=2)
+    fig.update_xaxes(range=[0, 21], row=1, col=3)
 
+    fig.update_yaxes(title_text=r'$\huge{\hat{\theta}^\dagger \cdot (\delta_d / \delta_s)}$', row=1, col=1, title_standoff=30)
 
-fig.update_yaxes(range=[-0.125, 5.625], row=1, col=1)
-fig.update_yaxes(range=[-0.125, 5.625], row=1, col=2)
-fig.update_yaxes(range=[-0.125, 5.625], row=1, col=3)
-fig.add_hline(y=1, line_width=1, line_dash="dash", line_color="green", row=1, col=1)
-fig.add_hline(y=1, line_width=1, line_dash="dash", line_color="green", row=1, col=2)
-fig.add_hline(y=1, line_width=1, line_dash="dash", line_color="green", row=1, col=3)
+    fig.update_xaxes(title_text=r'$\huge{\delta_s \ [\text{min}]}$', row=1, col=1)
+    fig.update_xaxes(title_text=r'$\huge{\delta_s \ [\text{min}]}$', row=1, col=2)
+    fig.update_xaxes(title_text=r'$\huge{\delta_s \ [\text{min}]}$', row=1, col=3)
 
+    annotations = fig.layout.annotations
+    annotations[0]['y'] = 1.025
+    annotations[1]['y'] = 1.025
+    annotations[2]['y'] = 1.025
 
-fig.update_xaxes(range=[0, 21], row=1, col=1)
-fig.update_xaxes(range=[0, 21], row=1, col=2)
-fig.update_xaxes(range=[0, 21], row=1, col=3)
+    fig.update_layout(annotations=annotations,
+                      legend=dict(
+                          yanchor="top",
+                          y=0.99,
+                          xanchor="left",
+                          x=0.9125
+                      )
+                      )
 
-fig.update_yaxes(title_text=r'$\huge{\hat{\theta}^\dagger \cdot (\delta_d / \delta_s)}$', row=1, col=1, title_standoff=30)
-
-fig.update_xaxes(title_text=r'$\huge{\delta_s \ [\text{min}]}$', row=1, col=1)
-fig.update_xaxes(title_text=r'$\huge{\delta_s \ [\text{min}]}$', row=1, col=2)
-fig.update_xaxes(title_text=r'$\huge{\delta_s \ [\text{min}]}$', row=1, col=3)
-
-annotations = fig.layout.annotations
-annotations[0]['y'] = 1.025
-annotations[1]['y'] = 1.025
-annotations[2]['y'] = 1.025
-
-fig.update_layout(annotations=annotations,
-                  legend=dict(
-                      yanchor="top",
-                      y=0.99,
-                      xanchor="left",
-                      x=0.9125
-                  )
-                  )
+    fig.write_image('./images/realistic_curves.png', width=w, height=h, scale=1)
 
 
-fig.write_image('./results_nyc/scatter.png', width=w, height=h, scale=1)
-#
-#
-# mst_cost = results_df.mst_cost.max()
-# delta_list = np.linspace(0.5, 20, 40)
-# cost_list = [mst_cost * 60 / delta for delta in delta_list]
-# fig = px.line(x=delta_list, y=cost_list)
-# fig.update_yaxes(title_text=r'$\huge{\text{Cost} \ [\text{m}/\text{hr}]}$')
-# fig.update_xaxes(title_text=r'$\huge{\delta \ [\text{min}]}$')
-# fig.write_image('./results_nyc/mst_cost.png', scale=1)
-#
-#
-#
-# #
-# #
-# # for _, row in results_df.iterrows():
-# #     g = row.g
-# #     _map = folium.Map(location=(row.g.nodes[42421877]['y'], row.g.nodes[42421877]['x']), zoom_start=13.5)
-# #     for u, v, data in row.g.edges(data=True):
-# #         points = [(row.g.nodes[u]['y'], row.g.nodes[u]['x']), (row.g.nodes[v]['y'], row.g.nodes[v]['x'])]
-# #         folium.PolyLine(points, color='blue', weight=3, opacity=(np.exp(data['p'])-1)/(np.exp(1)-1)).add_to(_map)
-# #     _map.fit_bounds(bounds=[(40.70038507925808, -74.02759529869529), (40.763849522046876, -73.94755407175593)])
-# #     _map.save('./results_nyc/delta_t_{0}_rho_{1}_g.html'.format(row.delta_t, round(row.rho, 2)))
-# #
-# #
-# #
-#
-#
-# # --- old --- #
-#
-# # --- plot solution --- #
-# # g_copy = g.copy()
-# # delta_t = delta_t_l[2]
-# # for u, v, data in g_copy.edges(data=True):
-# #     data['p'] = p_dicts[delta_t][(u, v)]
-# # _map = folium.Map(location=(g_copy.nodes[42421877]['y'], g_copy.nodes[42421877]['x']))
-# # ps = [data['p'] for _, _, data in g_copy.edges(data=True)]
-# # for u, v, data in g_copy.edges(data=True):
-# #     points = [(g_copy.nodes[u]['y'], g_copy.nodes[u]['x']), (g_copy.nodes[v]['y'], g_copy.nodes[v]['x'])]
-# #     folium.PolyLine(points, color='blue', weight=3, opacity=data['p']).add_to(_map)
-# # _map.save('./solution.html')
-#
-# # --- plot stops --- #
-# # _map = folium.Map(location=(g.nodes[42421728]['y'], g.nodes[42421728]['x']))
-# # for u, data in g.nodes(data=True):
-# #     if data['stop']:
-# #         folium.Circle(radius=20, location=[data['y'], data['x']]).add_to(_map)
-# # _map.save('./stops.html')
-#
-#
-# # --- plot g --- #
-# # _map = folium.Map(location=(g.nodes[42421877]['y'], g.nodes[42421877]['x']))
-# # for u, v, data in g.edges(data=True):
-# #     points = [(g.nodes[u]['y'], g.nodes[u]['x']), (g.nodes[v]['y'], g.nodes[v]['x'])]
-# #     folium.PolyLine(points, color='blue', weight=2).add_to(_map)
-# # _map.save('./g.html')
-#
-# # two y axes
-# # import plotly.graph_objects as go
-# # from plotly.subplots import make_subplots
-# # fig = make_subplots(rows=1, cols=3, specs=[[{"secondary_y": True}, {"secondary_y": True}, {"secondary_y": True}]])
-# # for idx, rho in enumerate(results_df['rho'].unique()):
-# #     rho_df = results_df[results_df['rho'] == rho]
-# #     fig.add_trace(
-# #         go.Scatter(x=rho_df['delta_t'], y=rho_df['a'], name="yaxis data"),
-# #         secondary_y=False, row=1, col=idx+1
-# #     )
-# #     fig.add_trace(
-# #         go.Scatter(x=rho_df['delta_t'], y=rho_df['c'], name="yaxis2 data"),
-# #         secondary_y=True, row=1, col=idx+1
-# #     )
-# # fig.write_image('./results_nyc/scatter2.png', width=w, height=h, scale=1)
+def map_plotter():
+
+    results_df = pd.read_pickle('./realistic/results_df.pkl')
+
+    for _, row in results_df.iterrows():
+        _map = folium.Map(location=(row.g.nodes[42421877]['y'], row.g.nodes[42421877]['x']), zoom_start=13.5)
+        for u, v, data in row.g.edges(data=True):
+            points = [(row.g.nodes[u]['y'], row.g.nodes[u]['x']), (row.g.nodes[v]['y'], row.g.nodes[v]['x'])]
+            folium.PolyLine(points, color='blue', weight=3, opacity=(np.exp(data['p'])-1)/(np.exp(1)-1)).add_to(_map)
+        _map.fit_bounds(bounds=[(40.70038507925808, -74.02759529869529), (40.763849522046876, -73.94755407175593)])
+        _map.save('./images/delta_t_{0}_rho_{1}_g.html'.format(row.delta_t, round(row.rho, 2)))
+
+
+# main()
+# curve_plotter()
+# map_plotter()
